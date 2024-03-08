abb877c1552401da137654601b94af0b856dea24
kwan3217 <kwan3217@gmail.com>
2024-03-08 13:34:29 -0700

    Update to correctly download all channels

--- get_lpi.sh
+++ get_lpi.sh
@@ -4,7 +4,8 @@
 # Local folder to store data
 LOCAL_ROOT=/mnt/big/workspace/Data/Ranger
 
-# Camera B
+# Camera A
+rm -rfv $LOCAL_ROOT/7A
 mkdir -pv $LOCAL_ROOT/7A
 for i in `seq -w 1 199`
 do
@@ -12,15 +13,18 @@ do
 done
 
 # Camera B
+rm -rfv $LOCAL_ROOT/7B
 mkdir -pv $LOCAL_ROOT/7B
-for i in `seq -w 1 190`
+for i in `seq -w 1 200`
 do
     wget -v https://www.lpi.usra.edu/resources/ranger/images/print/7/B/$i.jpg -O $LOCAL_ROOT/7B/Ranger7B$i.jpg
 done
 
-# Camera P
+# Camera P. There are 200 file, but only 190 are unique.
+# TABs 191-200 duplicate 181-190, with a different margin.
+rm -rfv $LOCAL_ROOT/7P
 mkdir -pv $LOCAL_ROOT/7P
-for i in `seq -w 1 200`
+for i in `seq -w 1 190`
 do
     wget -v https://www.lpi.usra.edu/resources/ranger/images/print/7/P/$i.jpg -O $LOCAL_ROOT/7P/Ranger7P$i.jpg
 done
