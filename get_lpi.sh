c11d173c62d18a547d46b1fb17b0972639124bbf
kwan3217 <kwan3217@gmail.com>
2024-03-23 22:22:14 -0600

    Get images for all cameras of all missions. Write to an internal folder rather than a link to my data folder (which you probably don't have).

--- get_lpi.sh
+++ get_lpi.sh
@@ -2,31 +2,105 @@
 #Script to get, resize, and convert all Ranger 7 camera images from the Lunar and Planetary Institute
 
 # Local folder to store data
-LOCAL_ROOT=/mnt/big/workspace/Data/Ranger
+LOCAL_ROOT=./images
 
+#Ranger 7
+MISSION=7
 # Camera A
-rm -rfv $LOCAL_ROOT/7A
-mkdir -pv $LOCAL_ROOT/7A
-for i in `seq -w 1 199`
+CAMERA=A
+IMAGES=199
+rm -rfv $LOCAL_ROOT/$MISSION$CAMERA
+mkdir -pv $LOCAL_ROOT/$MISSION$CAMERA
+for i in `seq -w 1 $IMAGES`
 do
-    wget -v https://www.lpi.usra.edu/resources/ranger/images/print/7/A/$i.jpg -O $LOCAL_ROOT/7A/Ranger7A$i.jpg
+    wget -v https://www.lpi.usra.edu/resources/ranger/images/print/$MISSION/$CAMERA/$i.jpg -O $LOCAL_ROOT/$MISSION$CAMERA/Ranger$MISSION$CAMERA$i.jpg
 done
 
 # Camera B
-rm -rfv $LOCAL_ROOT/7B
-mkdir -pv $LOCAL_ROOT/7B
-for i in `seq -w 1 200`
+CAMERA=B
+IMAGES=200
+rm -rfv $LOCAL_ROOT/$MISSION$CAMERA
+mkdir -pv $LOCAL_ROOT/$MISSION$CAMERA
+for i in `seq -w 1 $IMAGES`
 do
-    wget -v https://www.lpi.usra.edu/resources/ranger/images/print/7/B/$i.jpg -O $LOCAL_ROOT/7B/Ranger7B$i.jpg
+    wget -v https://www.lpi.usra.edu/resources/ranger/images/print/$MISSION/$CAMERA/$i.jpg -O $LOCAL_ROOT/$MISSION$CAMERA/Ranger$MISSION$CAMERA$i.jpg
 done
 
 # Camera P. There are 200 file, but only 190 are unique.
 # TABs 191-200 duplicate 181-190, with a different margin.
-rm -rfv $LOCAL_ROOT/7P
-mkdir -pv $LOCAL_ROOT/7P
-for i in `seq -w 1 190`
+CAMERA=P
+IMAGES=190
+rm -rfv $LOCAL_ROOT/$MISSION$CAMERA
+mkdir -pv $LOCAL_ROOT/$MISSION$CAMERA
+for i in `seq -w 1 $IMAGES`
 do
-    wget -v https://www.lpi.usra.edu/resources/ranger/images/print/7/P/$i.jpg -O $LOCAL_ROOT/7P/Ranger7P$i.jpg
+    wget -v https://www.lpi.usra.edu/resources/ranger/images/print/$MISSION/$CAMERA/$i.jpg -O $LOCAL_ROOT/$MISSION$CAMERA/Ranger$MISSION$CAMERA$i.jpg
+done
+
+
+#Ranger 8
+MISSION=8
+# Camera A
+CAMERA=A
+IMAGES=60
+rm -rfv $LOCAL_ROOT/$MISSION$CAMERA
+mkdir -pv $LOCAL_ROOT/$MISSION$CAMERA
+for i in `seq -f "%03.0f" $IMAGES`
+do
+    wget -v https://www.lpi.usra.edu/resources/ranger/images/print/$MISSION/$CAMERA/$i.jpg -O $LOCAL_ROOT/$MISSION$CAMERA/Ranger$MISSION$CAMERA$i.jpg
+done
+
+# Camera B
+CAMERA=B
+IMAGES=90
+rm -rfv $LOCAL_ROOT/$MISSION$CAMERA
+mkdir -pv $LOCAL_ROOT/$MISSION$CAMERA
+for i in `seq -f "%03.0f" $IMAGES`
+do
+    wget -v https://www.lpi.usra.edu/resources/ranger/images/print/$MISSION/$CAMERA/$i.jpg -O $LOCAL_ROOT/$MISSION$CAMERA/Ranger$MISSION$CAMERA$i.jpg
+done
+
+# Camera P
+CAMERA=P
+IMAGES=20
+rm -rfv $LOCAL_ROOT/$MISSION$CAMERA
+mkdir -pv $LOCAL_ROOT/$MISSION$CAMERA
+for i in `seq -f "%03.0f" $IMAGES`
+do
+    wget -v https://www.lpi.usra.edu/resources/ranger/images/print/$MISSION/$CAMERA/$i.jpg -O $LOCAL_ROOT/$MISSION$CAMERA/Ranger$MISSION$CAMERA$i.jpg
+done
+
+
+#Ranger 9
+MISSION=9
+# Camera A
+CAMERA=A
+IMAGES=70
+rm -rfv $LOCAL_ROOT/$MISSION$CAMERA
+mkdir -pv $LOCAL_ROOT/$MISSION$CAMERA
+for i in `seq -f "%03.0f" $IMAGES`
+do
+    wget -v https://www.lpi.usra.edu/resources/ranger/images/print/$MISSION/$CAMERA/$i.jpg -O $LOCAL_ROOT/$MISSION$CAMERA/Ranger$MISSION$CAMERA$i.jpg
+done
+
+# Camera B
+CAMERA=B
+IMAGES=88
+rm -rfv $LOCAL_ROOT/$MISSION$CAMERA
+mkdir -pv $LOCAL_ROOT/$MISSION$CAMERA
+for i in `seq -f "%03.0f" $IMAGES`
+do
+    wget -v https://www.lpi.usra.edu/resources/ranger/images/print/$MISSION/$CAMERA/$i.jpg -O $LOCAL_ROOT/$MISSION$CAMERA/Ranger$MISSION$CAMERA$i.jpg
+done
+
+# Camera P
+CAMERA=P
+IMAGES=12
+rm -rfv $LOCAL_ROOT/$MISSION$CAMERA
+mkdir -pv $LOCAL_ROOT/$MISSION$CAMERA
+for i in `seq -f "%03.0f" $IMAGES`
+do
+    wget -v https://www.lpi.usra.edu/resources/ranger/images/print/$MISSION/$CAMERA/$i.jpg -O $LOCAL_ROOT/$MISSION$CAMERA/Ranger$MISSION$CAMERA$i.jpg
 done
 
 
